10360489cd4d02d768ea193c3b6699f71af96264
topiniu <topiniu@outlook.com>
2017-08-14 10:29:54 +0800

    关闭tomcat测试

--- src/js/PASSAGELIST.js
+++ src/js/PASSAGELIST.js
@@ -9,7 +9,6 @@ export default function () {
      * Type your new passage here!
      * Titile  content  createDate  updateDate  type(FrontEnd or Music or Photography)
      */
-    // new Passage('Test', testFile, '2017-08-08', '2017-08-09', 'Front-end'),
     // new Passage('Test', testFile, '2017-08-08', '2017-08-09', 'Photography'),
     // new Passage('Test', testFile, '2017-08-08', '2017-08-09', 'Photography'),
     // new Passage('Test', testFile, '2017-08-08', '2017-08-09', 'Photography'),
