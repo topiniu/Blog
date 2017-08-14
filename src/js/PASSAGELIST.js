9877692874c915d0e269fcc16968cc6e26d20d9f
topiniu <topiniu@outlook.com>
2017-08-14 16:38:02 +0800

    添加一片新文章-四个月在尚妆

--- src/js/PASSAGELIST.js
+++ src/js/PASSAGELIST.js
@@ -2,6 +2,7 @@ import Passage from './passageEntity.js';
 
 import testFile from '../md/testFile.md';
 import showjoy_guide_1 from '../md/showjoy_guide1.md';
+import in_showjoy_1 from '../md/inshowjoy_1.md'
 
 export default function () {
   const list = [
@@ -20,7 +21,8 @@ export default function () {
     // new Passage('Test', testFile, '2017-08-08', '2017-08-09', 'Music'),
     // new Passage('Test', testFile, '2017-08-08', '2017-08-09', 'Music'),
     // new Passage('Test', testFile, '2017-08-08', '2017-08-09', 'Music'),
-    new Passage('尚妆前端快速上手-环境搭建', showjoy_guide_1, '2017-08-11', '2017-08-11', 'Front-end')
+    new Passage('尚妆前端快速上手-环境搭建', showjoy_guide_1, '2017-08-11', '2017-08-11', 'Front-end'),
+    new Passage('四个月在尚妆', in_showjoy_1, '2017-08-14', '2017-08-14', 'Front-end'),
 
 
 
