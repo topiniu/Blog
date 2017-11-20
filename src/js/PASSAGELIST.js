8a0a8d83e82251a6a7ad27269bd4e64633ee3ccb
topiniu <topiniu@outlook.com>
2017-11-20 09:58:19 +0800

    修改文章顺序

--- src/js/PASSAGELIST.js
+++ src/js/PASSAGELIST.js
@@ -18,13 +18,13 @@ export default function () {
      * Type your new passage here!
      * Titile  content  createDate  updateDate  type(FrontEnd or Music or Photography)
      */
+    new Passage('从零编写一个简易待办小程序-P1', todo_by_wx, '2017-11-13', '2017-11-13', 'Front-end'),
     new Passage('尚妆前端快速上手-环境搭建', showjoy_guide_1, '2017-08-11', '2017-08-11', 'Front-end'),
     new Passage('三个月在尚妆', in_showjoy_1, '2017-08-14', '2017-08-14', 'Front-end'),
     new Passage('尚妆前端快速上手-项目概览', showjoy_guide_2, '2017-08-16', '2017-08-16', 'Front-end'),
     new Passage('使用Git hook实现直接部署', usegithook, '2017-08-17', '2017-08-17', 'Front-end'),
     new Passage('JavaScript高级程序设计读书笔记-P1', jsbase1, '2017-08-21', '2017-08-28', 'Front-end'),
     new Passage('JavaScript高级程序设计读书笔记-P2', jsbase2, '2017-08-28', '2017-08-28', 'Front-end'),
-    new Passage('从零编写一个简易待办小程序-P1', todo_by_wx, '2017-11-13', '2017-11-13', 'Front-end'),
     
   ];
   return list;
